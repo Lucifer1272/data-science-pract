95974fbbc9d4a7dd9983080a794137cfd08114ba
FarhaanD <81569325+Lucifer1272@users.noreply.github.com>
2024-03-03 20:02:07 +0530

    Add files via upload

---
+++ sc_pract1.py
@@ -0,0 +1,36 @@
+from scipy.misc import imread
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+# Input Agreement ============================================
+sInputFileName="D:/1_UPG_MSCIT_PART1/Data_Science_Practical/Practical_Files/Angus"
+InputData = imread(sInputFileName, flatten=False, mode='RGBA')
+
+print('Input Data Values ===================================')
+print('X: ',InputData.shape[0])
+print('Y: ',InputData.shape[1])
+print('RGBA: ', InputData.shape[2])
+print('=====================================================')
+# Processing Rules ===========================================
+ProcessRawData=InputData.flatten()
+y=InputData.shape[2] + 2
+x=int(ProcessRawData.shape[0]/y)
+ProcessData=pd.DataFrame(np.reshape(ProcessRawData, (x, y)))
+sColumns= ['XAxis','YAxis','Red', 'Green', 'Blue','Alpha']
+ProcessData.columns=sColumns
+ProcessData.index.names =['ID']
+print('Rows: ',ProcessData.shape[0])
+print('Columns :',ProcessData.shape[1])
+print('=====================================================')
+print('Process Data Values =================================')
+print('=====================================================')
+plt.imshow(InputData)
+plt.show() 
+print('=====================================================')
+# Output Agreement ===========================================
+OutputData=ProcessData
+print('Storing File')
+sOutputFileName='D:/1_UPG_MSCIT_PART1/Data_Science_Practical/Practical_Files/HORUS-Picture.csv'
+OutputData.to_csv(sOutputFileName, index = False)
+print('=====================================================')
+print('Picture to HORUS - Done')
