b30c0bf5709bdb5c8d991578325d2da4004bd39b
FarhaanD <81569325+Lucifer1272@users.noreply.github.com>
2024-03-04 23:32:48 +0530

    Update Calculate the output of neural net where input X = 0.2, w = 0.3 and bias b 0.45.py

--- Calculate the output of neural net where input X = 0.2, w = 0.3 and bias b 0.45.py
+++ Calculate the output of neural net where input X = 0.2, w = 0.3 and bias b 0.45.py
@@ -1,6 +1,14 @@
-inputs = float(input("Enter the input: ")) weights = float(input("Enter the weight: ")) bias = float(input("Enter bias: "))
+#Yin = wx + b = 0.3*0.2 + 0.45 = 0.51.
+
+inputs = float(input("Enter the input: ")) 
+weights = float(input("Enter the weight: ")) 
+bias = float(input("Enter bias: "))
 print(' ')
-yin = bias + (inputs * weights) if yin < 0:
-out = 0 elif yin > 1: out= 1
+yin = bias + (inputs * weights) 
+if yin < 0:
+    out = 0 
+elif yin > 1: out= 1
 else:
-out = yin print("Output is: ",out) 
+    out = yin 
+print("Output is: ",out) 
+print(' ')
